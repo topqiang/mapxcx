13b675aa9b1b3e2be95d473e81245c117afdd640
topqiang <topqiang@kanziqiang.top>
2017-06-12 11:49:39 +0800

    0612

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -18,10 +18,10 @@ return  array(
     'COOKIE_PREFIX'         => 'NONGYE', // Cookie前缀 避免冲突
 
     /* 数据库设置 */
-    'DB_TYPE'               => 'mysqli', // 数据库类型
+    'DB_TYPE'               => 'mysql', // 数据库类型
     'DB_HOST'               => 'localhost', // 服务器地址
     'DB_USER'               => 'root', // 用户名
-    'DB_PWD'                => '123456', // 密码
+    'DB_PWD'                => 'shebei2017', // 密码
     'DB_NAME'               => 'jians', // 数据库名
     'DB_PORT'               => '3306', // 端口
     'DB_PREFIX'             => 'js_', // 数据库表前缀
