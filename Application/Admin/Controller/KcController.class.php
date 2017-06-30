48d163f6f40015d353af9f8cc290612f22bcedd0
topqiang <topqiang@kanziqiang.top>
2017-06-30 19:41:45 +0800

    0630

--- Application/Admin/Controller/KcController.class.php
+++ Application/Admin/Controller/KcController.class.php
@@ -60,6 +60,22 @@ class KcController extends AdminBasicController {
         }
     }
 
+    public function deletejkc(){
+
+        if(empty($_REQUEST['id'])){
+            $this->error('您未选择任何操作对象');
+        }
+        $data['id'] = array('IN',I('request.id'));
+        $data['status'] = 9;
+        $upd_res = $this -> jkc -> save( $data );
+        
+        if($upd_res){
+            $this->success('删除操作成功');
+        }else{
+            $this->error('删除操作失败');
+        }
+    }
+
     public function addkc(){
         if(!IS_POST){
             $this->display("addkc");
