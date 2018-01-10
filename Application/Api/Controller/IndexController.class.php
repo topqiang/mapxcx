f4f9efdb99159149e72f9b084246e8145899d7a0
topqiang <topqiang@kanziqiang.top>
2018-01-10 09:21:30 +0800

    0110

--- Application/Api/Controller/IndexController.class.php
+++ Application/Api/Controller/IndexController.class.php
@@ -11,7 +11,7 @@ class IndexController extends Controller {
 		$data['uid'] = $_POST['uid'];
 		$data['latitude'] = $_POST['latitude'];
 		$data['longitude'] = $_POST['longitude'];
-		$data['keyword'] = $_POST['keyword'];
+		$data['keyword'] = $this -> filter_mark($_POST['keyword']);
 		$data['ctime'] = time();
 		$data['status'] = 0;
 		
@@ -31,6 +31,15 @@ class IndexController extends Controller {
 		}
 		print json_encode($res1);
 	}
+
+	function filter_mark($text){ 
+		if(trim($text)=='')return ''; 
+		$text=preg_replace("/[[:punct:]\s]/",' ',$text); 
+		$text=urlencode($text); 
+		$text=preg_replace("/(%7E|%60|%21|%40|%23|%24|%25|%5E|%26|%27|%2A|%28|%29|%2B|%7C|%5C|%3D|\-|_|%5B|%5D|%7D|%7B|%3B|%22|%3A|%3F|%3E|%3C|%2C|\.|%2F|%A3%BF|%A1%B7|%A1%B6|%A1%A2|%A1%A3|%A3%AC|%7D|%A1%B0|%A3%BA|%A3%BB|%A1%AE|%A1%AF|%A1%B1|%A3%FC|%A3%BD|%A1%AA|%A3%A9|%A3%A8|%A1%AD|%A3%A4|%A1%A4|%A3%A1|%E3%80%82|%EF%BC%81|%EF%BC%8C|%EF%BC%9B|%EF%BC%9F|%EF%BC%9A|%E3%80%81|%E2%80%A6%E2%80%A6|%E2%80%9D|%E2%80%9C|%E2%80%98|%E2%80%99|%EF%BD%9E|%EF%BC%8E|%EF%BC%88)+/",' ',$text); 
+		$text=urldecode($text); 
+		return trim($text); 
+	} 
 
 	public function index(){
 		$where['uid'] = $_POST['uid'];
