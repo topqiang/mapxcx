699650bfbc7540ba43ae3bbe092c7f20372b1326
topqiang <topqiang@kanziqiang.top>
2018-01-23 21:27:43 +0800

    用户列表分页

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -19,13 +19,18 @@ class UserController extends AdminBasicController {
      */
     public function userlist(){
         $w['status'] = array('neq',9);
-        
-        if($_REQUEST['nick_name']){
+        $name = $_REQUEST['nick_name'];
+        if($name){
             $w['uname'] = array('LIKE','%'.$_REQUEST['nick_name'].'%');
         }
-
-        $list = $this -> user -> where($w) -> order('addtime desc') -> select();
-        $this->assign("list",$list);
+        $count =  $this -> user -> where($where)->count();
+        $page = new \Think\Page($count,15);
+        if ($name) {
+            $page->parameter['nick_name'] =  $name;
+        }
+        $userlist = $this -> user -> where($w) -> order('addtime desc') -> limit($page->firstRow,$page->listRows) -> select();
+        $this -> assign('list',$userlist);
+        $this -> assign('page',$page->show());
         $this->display();
     }
 }
